1bdf0299310530d43803310380aa6c60ddba43e9
Hunter Lightman <git.huntrr@gmail.com>
2016-02-27 12:50:17 -0500

    Step 4: Planets!

--- script.js
+++ script.js
@@ -9,9 +9,12 @@ $(document).ready( function() {
 
   var $sun = $('#sun');
   var $bg = $('#background');
+  var $fg = $('#foreground');
 
   var width = $(window).width();
   var height = $(window).height();
+  var sunX = 0;
+  var sunY = 0;
 
   var stars = [];
   var planets = [];
@@ -25,10 +28,13 @@ $(document).ready( function() {
     width = $(window).width();
     height = $(window).height();
 
-    var sunX = (width / 2) - ($sun.width() / 2);
-    var sunY = (height / 2) - ($sun.height() / 2);
-    $sun.css('left', sunX);
-    $sun.css('top', sunY);
+    sunX = (width / 2);
+    sunY = (height / 2);
+
+    var x = sunX - ($sun.width() / 2);
+    var y = sunY - ($sun.height() / 2);
+    $sun.css('left', x);
+    $sun.css('top', y);
   }
 
   /**
@@ -37,8 +43,8 @@ $(document).ready( function() {
   var createStar = function() {
     var newStar = $('<img class="star" src="img/star.png" />');
 
-    var x = Math.random() * width;
-    var y = Math.random() * height;
+    x = Math.random() * width;
+    y = Math.random() * height;
     newStar.css('left', x);
     newStar.css('top', y);
 
@@ -53,10 +59,6 @@ $(document).ready( function() {
     }
   }
 
-  //for(var i = 0; i < MAX_STARS; i++) {
-    //createStar();
-  //}
-
 
   var loop = setInterval(function() {
     starTimer += INTERVAL;
@@ -66,10 +68,79 @@ $(document).ready( function() {
       createStar();
     }
 
+    for(var p = 0; p < planets.length; p++) {
+      planets[p].update();
+    }
   }, INTERVAL);
 
   $(window).resize(function(e) {
     positionElements();
   });
   positionElements();
+
+  
+  var addPlanet = function() {
+    planets.push(PlanetFactory());
+  }
+  
+  var removePlanet = function() {
+    if(planets.length > 0) {
+      planets.pop().remove();
+    }
+  }
+
+  $(window).keypress(function(e) {
+    if(e.keyCode === 97) {
+      addPlanet();
+    }
+
+    if(e.keyCode === 122) {
+      removePlanet();
+    }
+  });
+
+
+
+  function PlanetFactory() {
+    var type = Math.floor(Math.random() * 6) + 1;
+    var distance = Math.random() * (width / 2 - $sun.width()) + $sun.width() / 2;
+    var speed = Math.random() * 0.06;
+    var size = Math.random() * 30 + 20;
+
+    return Planet(type, distance, speed, size);
+  }
+  
+  function Planet(type, distance, speed, size) {
+    var t = Math.random() * 2 * Math.PI;
+    var $elem = $('<img class="planet" src="img/' + type + '.png" />');
+    $elem.css('width', size);
+    var x = 0;
+    var y = 0;
+
+    $fg.append($elem);
+
+    var updatePos = function() {
+      x = Math.cos(t) * distance + sunX - $elem.width()/2;
+      y = Math.sin(t) * distance + sunY - $elem.width()/2;
+    }
+
+    var draw = function() {
+      $elem.css('left', x);
+      $elem.css('top', y);
+    }
+
+    return {
+      update: function() {
+        t += speed;
+
+        pos = updatePos();
+        draw();
+      },
+      remove: function() {
+        $elem.remove();
+      }
+    };
+  }
 } );
+
+
